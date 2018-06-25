88254d95404bc79a840d7d2c91d414ed307c496c
AnastasiaPostanogova <35628928+AnastasiaPostanogova@users.noreply.github.com>
2018-06-25 21:17:17 +0500

    Full

---
+++ 3_Задача/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            bool ok;
+            do
+            {
+                Console.WriteLine("Введите число Х");
+                string buf = Console.ReadLine();
+                ok = double.TryParse(buf, out x);
+                if (!ok)
+                    Console.WriteLine("Ошибка");
+            }
+            while (!ok);
+            if (x <= 0) y = -x;
+                else if ((x > 0) && (x < 1)) y = x;
+                    else if ((x >= 1) && (x <= 2)) y = 1;
+                        else y=-2*x + 5;
+            Console.WriteLine("Решением примера является число: {0}", y);
+        }
+    }
+}
